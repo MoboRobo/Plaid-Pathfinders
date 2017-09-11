f5a51e1ab16c4bb291b8b7fefdee1bac927cff4f
ben-soudry <soudryb@gmail.com>
2017-09-10 20:16:37 -0400

    Lab2 for loops removed

--- Lab2_Ben.m
+++ Lab2_Ben.m
@@ -8,53 +8,28 @@ rangeArray = zeros(1,360)
 xArray = zeros(1,360)
 yArray = zeros(1,360)
 thArray = zeros(1,360)
-maxObjectRange = 1.0
+
 iArray = zeros(1,360)
 
 iArray = 1:360 
 
+maxObjectRange = 1.0
 minObjectRange = 0.06
+idealObjectRange = 0.5
 
-while(iterations < 10)
+while(iterations < 200)
     rangeArray = robot.laser.LatestMessage.Ranges
+    filter = rangeArray > minObjectRange & rangeArray < maxObjectRange
+    filterRange = rangeArray(filter)
+    filterIndex = rangeArray(filter)
     
-    %[xArray, yArray,thArray] = irToXy(iArray,rangeArray) %doesnt work
-     for i = 1:numel(rangeArray) %slow
-        [xArray(i), yArray(i),thArray(i)] = irToXy(i,rangeArray(i))
-     end
-    %{
-    filteredX = xArray
-    filteredY = yArray
+    [minRange, minIndex] = min(filterRange)
     
-    for i = 1:numel(rangeArray)
-        if(rangeArray(i) > maxObjectRange | rangeArray(i) < minObjectRange)
-            filteredX(i) = NaN
-            filteredY(i) = NaN
-        end
-    end
-    %}
+    [xArray, yArray,thArray] = irToXy(iArray,rangeArray') 
     
-    minRange = 100
-    minIndex  = 0
-    for i = 1:numel(rangeArray)
-        if(rangeArray(i) < minRange && rangeArray(i) > minObjectRange)
-            minRange = rangeArray(i)
-            minIndex = i
-        end
-    end
+
     
-    if(minRange < 0.2 | minRange > maxObjectRange)
-        %robot.sendVelocity(0.0, 0.0)
-    else
-        pError =  xArray(minIndex) 
-        %Object Left = positive, Object Right = negative
-        kp = 0.2
-        leftVel = max([min([0.1+kp*pError, 0.5]),0.0])
-        rightVel = max([min([0.1-kp*pError, 0.5]),0.0])
-        %robot.sendVelocity(leftVel, rightVel)
-    end
- 
-    plot(xArray(minIndex),yArray(minIndex),'X')
+    plot(xArray(filter),yArray(filter),'X')
     axis([-1 1 -1 1])
     iterations = iterations + 1
     pause(0.05)
