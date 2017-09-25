c997b1b39f36762540f64825461024bd4e310ad0
zCoCo <connor.caxd@gmail.com>
2017-09-24 21:11:45 -0400

    Finalized API Reunification

--- Tests/Level 3/plot_figure8_INTEGRATEDPLOT_test.m
+++ Tests/Level 3/plot_figure8_INTEGRATEDPLOT_test.m
@@ -1,4 +1,4 @@
-function plot_circle_test(robot_id)
+function plot_figure8_INTEGRATEDPLOT_test(robot_id)
     %% SETUP ROBOT
     rasp = raspbot(robot_id, [0; 0; pi/2])
     rob = P2_Robot(rasp);
@@ -29,7 +29,7 @@ function plot_circle_test(robot_id)
     T_f = k_s*t_f;
     
     %% ALGORITHM
-    %rob.enablePositionPlotting();
+    rob.enablePositionPlotting();
     
     rob.waitForReady();
     
