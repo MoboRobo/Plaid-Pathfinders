5dbe898781d78570fd61104528894ee5f5debffc
Roman Kaufman <injuredroman@Romans-MBP.wv.cc.cmu.edu>
2017-10-02 00:45:12 -0400

    PID with homogeneous transforms

--- P2_Toolbox/planning/@PID/PID.m
+++ P2_Toolbox/planning/@PID/PID.m
@@ -6,10 +6,12 @@ classdef PID < handle
         rob;
         
         %error values
-        lastErrorVel = 0;
-        lastErrorOm = 0;
-        errorIntegralVel = 0;
-        errorIntegralOm = 0;
+        lastErrorX = 0;
+        lastErrorY = 0;
+        lastErrorTh = 0;
+        errorIntegralX = 0;
+        errorIntegralY = 0;
+        errorIntegralTh = 0;
         
         %history of error readings stored as poses
         error_poses = [pose(0, 0, 0)];
@@ -20,8 +22,9 @@ classdef PID < handle
         % maximum values
         v_max = 0.2;
         w_max = 2*pi;
-        maxErrorIntegralVel = 10;
-        maxErrorIntegralOm = 10;
+        maxErrorIntegralX = 10;
+        maxErrorIntegralY = 10;
+        maxErrorIntegralTh = 10;
         
         %PID coefficients
         k_p = 3;
@@ -58,41 +61,48 @@ classdef PID < handle
             k_th = 1 / obj.correctiveTime;
             k_y = 2 / (abs(V) * obj.correctiveTime^2);
             wrp = (refPose.poseVec(1:2) - curPose.poseVec(1:2));
-            thr = curPose.th;
+            thr = atan2(sin(curPose.th), cos(curPose.th));
             errorTh = atan2(sin(refPose.th), cos(refPose.th)) - ...
                 atan2(sin(curPose.th), cos(curPose.th));
             rrp = inv([cos(thr), -sin(thr); sin(thr), cos(thr)]) * wrp
             errorX = rrp(1);
             errorY = rrp(2);
             errorTh = atan2(sin(errorTh), cos(errorTh)); % normalize angle
-            errorVel = k_x * errorX;
-            errorOm = k_y * errorY + k_th * errorTh;
             dt = t - t_last;
-            tt.hist_encError.x = errorX;
 
             %% compute derivatives and integrals
-            errorDerivativeVel = (errorVel-obj.lastErrorVel)/ dt;
-            errorDerivativeOm = (errorOm - obj.lastErrorOm) / dt;
-            obj.errorIntegralOm = obj.errorIntegralOm + errorOm*dt;
-            if abs(obj.errorIntegralOm) > obj.maxErrorIntegralOm;
-                sign = obj.errorIntegralOm/abs(obj.errorIntegralOm);
-                obj.errorIntegralOm= obj.maxErrorIntegralOm * sign;
+            errorDerivativeX = (errorX-obj.lastErrorX)/ dt;
+            errorDerivativeY = (errorY - obj.lastErrorY) / dt;
+            errorDerivativeTh = (errorTh - obj.lastErrorTh) / dt;
+            obj.errorIntegralX = obj.errorIntegralX + errorX*dt;
+            if abs(obj.errorIntegralX) > obj.maxErrorIntegralX;
+                sign = obj.errorIntegralX/abs(obj.errorIntegralX);
+                obj.errorIntegralX= obj.maxErrorIntegralX * sign;
             end
             
-            obj.errorIntegralVel = obj.errorIntegralVel + errorVel*dt;
-            if abs(obj.errorIntegralVel) > obj.maxErrorIntegralVel;
-                sign = obj.errorIntegralVel/abs(obj.errorIntegralVel);
-                obj.errorIntegralVel= obj.maxErrorIntegralVel * sign;
+            obj.errorIntegralY = obj.errorIntegralY + errorY*dt;
+            if abs(obj.errorIntegralY) > obj.maxErrorIntegralY;
+                sign = obj.errorIntegralY/abs(obj.errorIntegralY);
+                obj.errorIntegralY= obj.maxErrorIntegralY * sign;
             end
 
-
+            obj.errorIntegralTh = obj.errorIntegralTh + errorTh*dt;
+            if abs(obj.errorIntegralTh) > obj.maxErrorIntegralTh;
+                sign = obj.errorIntegralTh/abs(obj.errorIntegralTh);
+                obj.errorIntegralTh= obj.maxErrorIntegralTh * sign;
+            end
+            
+            ex = obj.k_p * errorX + obj.k_d * errorDerivativeX + ...
+                obj.k_i * obj.errorIntegralX
+            ey = obj.k_p * errorY + obj.k_d * errorDerivativeY + ...
+                obj.k_i * obj.errorIntegralY
+            eth = obj.k_p * errorTh + obj.k_d * errorDerivativeTh + ...
+                obj.k_i * obj.errorIntegralTh
             %% compute actual control linear and rotational velocity
-            u_v = obj.k_p * errorVel + obj.k_d * errorDerivativeVel + ...
-                obj.k_i*obj.errorIntegralVel;
-            u_w = obj.k_p * errorOm + obj.k_d * errorDerivativeOm + ...
-                obj.k_i * obj.errorIntegralOm;
-            u_v = errorVel;
-            u_w = errorOm;
+%             u_v = ex * k_x;
+%             u_w = ey * k_y + eth * k_th;
+            u_v = errorX * k_x;
+            u_w = errorY * k_y + errorTh*k_th;
             %% ensure below ceiling
             if abs(u_v) > obj.v_max
                 sign = u_v / abs(u_v);
