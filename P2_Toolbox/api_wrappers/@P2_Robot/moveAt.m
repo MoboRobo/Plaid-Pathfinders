993a5cec74abe758c4a29fd1630146981c40eadc
Roman Kaufman <roman.a.kaufman@gmail.com>
2017-10-05 19:09:23 -0400

    small changes

--- P2_Toolbox/api_wrappers/@P2_Robot/moveAt.m
+++ P2_Toolbox/api_wrappers/@P2_Robot/moveAt.m
@@ -4,6 +4,9 @@ function moveAt(obj, V, omega)
     v_r = V + (obj.WHEEL_TREAD/2)*omega;
     v_l = V - (obj.WHEEL_TREAD/2)*omega;
 
+    if (v_l > .5 || v_r > .5) 
+        fprintf('AAAAAAAA');
+    end
     obj.sendVelocity(v_l,v_r);
     
     obj.curr_V = V;
