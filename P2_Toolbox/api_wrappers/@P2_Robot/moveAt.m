484a3de6cac47b257ec8dfdc0c6c671efe5d09ae
Roman Kaufman <roman.a.kaufman@gmail.com>
2017-10-07 21:56:21 -0400

    small additions to moveAt

--- P2_Toolbox/api_wrappers/@P2_Robot/moveAt.m
+++ P2_Toolbox/api_wrappers/@P2_Robot/moveAt.m
@@ -4,8 +4,17 @@ function moveAt(obj, V, omega)
     v_r = V + (obj.WHEEL_TREAD/2)*omega;
     v_l = V - (obj.WHEEL_TREAD/2)*omega;
 
-    if (v_l > .5 || v_r > .5) 
-        fprintf('AAAAAAAA');
+    if (v_l > .5)
+        fprintf('v_l greater than max!');
+    end
+    if (v_r > .5)
+        fprintf('v_r greater than max!');
+    end
+    if (v_l < -.5)
+        fprintf('v_l less than min!');
+    end
+    if (v_r < -.5)
+        fprintf('v_r less than min!');
     end
     obj.sendVelocity(v_l,v_r);
     
