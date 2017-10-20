a735f503a0bb3fc27d34f8e371692c290b9afeda
zCoCo <connor.caxd@gmail.com>
2017-10-19 20:07:55 -0400

    Lab7

--- Project 7/Lab7.m
+++ Project 7/Lab7.m
@@ -5,6 +5,7 @@ function Lab7(robot_id)
      
      mrpl.plottingOn(); % Turn on Plotting
      Trajectory.plot_thetas(1); % Turn on Heading Plotting
+     %mrpl.debugging.error_plots = 1;
      
      mrpl.goTo_Rel( pose(0.3048,0.3048,0.0) );
      mrpl.goTo_Rel( pose(-0.6096,-0.6096,-pi/2.0) );
