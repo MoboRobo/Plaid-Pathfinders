6523474b7ffc68016b683d2e79e84ff2d893699c
zCoCo <connor.caxd@gmail.com>
2017-10-24 01:13:25 -0400

    Works? No.

--- Project 8/Lab8.m
+++ Project 8/Lab8.m
@@ -5,7 +5,9 @@ function Lab8(robot_id)
      
      pause(2); % Wait for Robot to Initialize
      
-     mrpl.goTo_Rel( mrpl.getNearestLineObject() );
+     p_nlo_r = mrpl.getNearestLineObject();
+     p_acq_r = mrpl.acquisitionPose(p_nlo_r, 0.067, 0.02, 0);
+     mrpl.goTo_Rel( p_acq_r );
      
      mrpl.rob.core.forksUp();
      
